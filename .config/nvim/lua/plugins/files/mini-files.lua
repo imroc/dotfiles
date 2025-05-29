4f83bf119907ec2bac87fb621e5ff96bbd23cab8
roc <roc@imroc.cc>
2025-05-29 17:41:46 +0800

    nvim config: add mini-files short keys
    Signed-off-by: roc <roc@imroc.cc>

--- .config/nvim/lua/plugins/files/mini-files.lua
+++ .config/nvim/lua/plugins/files/mini-files.lua
@@ -1,5 +1,9 @@
 return {
   "echasnovski/mini.files",
+  keys = {
+    { "<leader>m", "<leader>fm", desc = "[P] Open mini.files (Directory of Current File)", remap = true },
+    { "<leader>M", "<leader>fM", desc = "[P] Open mini.files (cwd)", remap = true },
+  },
   opts = {
     windows = {
       preview = false,
