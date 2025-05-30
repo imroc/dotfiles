b7ba8a9f2d05fdac8846e2f530ab23b9cf1062e0
roc <roc@imroc.cc>
2025-05-30 09:25:05 +0800

    fish: add private path
    Signed-off-by: roc <roc@imroc.cc>

---
+++ .config/fish/conf.d/private.fish
@@ -0,0 +1,12 @@
+set -l private_path ~/.config/private
+
+if not test -d $private_path
+    return
+end
+
+set fish_complete_path $fish_complete_path[1] $private_path/completions $fish_complete_path[2..]
+set fish_function_path $fish_function_path[1] $private_path/functions $fish_function_path[2..]
+
+for file in $private_path/conf.d/*.fish
+    source $file 2>/dev/null
+end
