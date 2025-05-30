df6d41cdd9f523e5d3339cc3c04d77cc4e0251a2
roc <roc@imroc.cc>
2025-05-30 11:21:02 +0800

    fish abbr: add v
    Signed-off-by: roc <roc@imroc.cc>

--- .config/fish/conf.d/editor.fish
+++ .config/fish/conf.d/editor.fish
@@ -2,3 +2,4 @@ abbr --add ys "kubectl neat > $HOME/tmp/kube.yaml; and nvim $HOME/tmp/kube.yaml"
 abbr --add yy "cat > $HOME/tmp/kube.yaml; and nvim $HOME/tmp/kube.yaml"
 abbr --add y 'nvim -c "set filetype=yaml"'
 abbr --add c "code -r"
+abbr --add v nvim
