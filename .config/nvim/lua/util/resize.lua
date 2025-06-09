5d985ee056fcb8275bf3e8b5312dff46119a8e9f
roc <roc@imroc.cc>
2025-06-09 12:11:34 +0800

    ajust nvim resize
    Signed-off-by: roc <roc@imroc.cc>

--- .config/nvim/lua/util/resize.lua
+++ .config/nvim/lua/util/resize.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local size = 5
+local size = 8
 
 local function is_window_on_right()
   local win = vim.api.nvim_get_current_win()
@@ -10,10 +10,13 @@ local function is_window_on_right()
   local right_pos = screen_width - col - width
   return right_pos <= 1
 end
-local function is_window_on_top()
+local function is_window_on_bottom()
   local win = vim.api.nvim_get_current_win()
   local row = vim.fn.win_screenpos(win)[1]
-  return row <= 2
+  local height = vim.api.nvim_win_get_height(win)
+  local screen_height = vim.o.lines
+  local bottom_pos = screen_height - row - height
+  return bottom_pos <= 1
 end
 
 function M.resize_width_left()
@@ -35,20 +38,20 @@ function M.resize_width_right()
 end
 
 function M.resize_height_down()
-  -- 只有最上边的 window 才变高，其它都变矮
-  if is_window_on_top() then
-    vim.cmd("resize +" .. size)
-  else
+  -- 只有最下边的 window 才变矮，其它都变高
+  if is_window_on_bottom() then
     vim.cmd("resize -" .. size)
+  else
+    vim.cmd("resize +" .. size)
   end
 end
 
 function M.resize_height_up()
-  -- 只有最上边的 window 才变矮，其它都变高
-  if is_window_on_top() then
-    vim.cmd("resize -" .. size)
-  else
+  -- 只有最边边的 window 才变高，其它都变矮
+  if is_window_on_bottom() then
     vim.cmd("resize +" .. size)
+  else
+    vim.cmd("resize -" .. size)
   end
 end
 
