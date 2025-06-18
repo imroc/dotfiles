46e4c3d7ed9cb516f2b151d17200c47193d1faaf
roc <roc@imroc.cc>
2025-06-18 16:41:32 +0800

    nvim: update mason
    Signed-off-by: roc <roc@imroc.cc>

--- .config/nvim/lua/plugins/editor/mason.lua
+++ .config/nvim/lua/plugins/editor/mason.lua
@@ -3,17 +3,37 @@ return {
     "mason-org/mason.nvim",
     opts = function(_, opts)
       vim.api.nvim_create_user_command("MasonInstallAll", function()
-        local config = require("mason.settings").current
-        for _, tool in ipairs(config.ensure_installed) do
-          vim.cmd("MasonInstall " .. tool)
+        -- Combine all packages into a single list
+        local packages_to_install = {}
+
+        -- Add packages from ensure_installed of mason.nvim
+        for _, tool in ipairs(require("mason.settings").current.ensure_installed) do
+          table.insert(packages_to_install, tool)
         end
-        local mason_lsp_config = require("mason-lspconfig.settings").current
+
+        -- Add packages from ensure_installed of mason-lspconfig.nvim
         local lspconfig_to_package = require("mason-lspconfig.mappings.server").lspconfig_to_package
-        for _, server in ipairs(mason_lsp_config.ensure_installed) do
+        for _, server in ipairs(require("mason-lspconfig.settings").current.ensure_installed) do
           local pkg = lspconfig_to_package[server]
           if pkg then
-            vim.cmd("MasonInstall " .. pkg)
+            table.insert(packages_to_install, pkg)
           end
+        end
+
+        -- Remove duplicates
+        local seen = {}
+        local unique_packages = {}
+        for _, pkg in ipairs(packages_to_install) do
+          if not seen[pkg] then
+            seen[pkg] = true
+            table.insert(unique_packages, pkg)
+          end
+        end
+
+        -- Install all unique packages
+        for _, pkg in ipairs(unique_packages) do
+          vim.notify("Installing " .. pkg, vim.log.levels.INFO)
+          vim.cmd("MasonInstall " .. pkg)
         end
       end, {})
       return opts
