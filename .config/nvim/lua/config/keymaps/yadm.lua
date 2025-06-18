aee2aa02530eaac8697ef9acabf24f37608b94f8
roc <roc@imroc.cc>
2025-06-18 21:25:02 +0800

    nvim: <leader>yl --> <leader>yg

--- .config/nvim/lua/config/keymaps/yadm.lua
+++ .config/nvim/lua/config/keymaps/yadm.lua
@@ -1,4 +1,4 @@
 local yadm = require("util.yadm")
 
-vim.keymap.set("n", "<leader>yl", yadm.open_lazygit, { desc = "[P]Open Lazygit (yadm)" })
+vim.keymap.set("n", "<leader>yg", yadm.open_lazygit, { desc = "[P]Open Lazygit (yadm)" })
 vim.keymap.set("n", "<leader>ye", yadm.set_git_env, { desc = "[P]Set Git Env (yadm work tree)" })
