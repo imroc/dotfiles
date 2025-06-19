b6c9b5fb12356011e6f163511b7d0d1602009df2
roc <roc@imroc.cc>
2025-06-19 10:53:27 +0800

    groot
    Signed-off-by: roc <roc@imroc.cc>

--- .config/fish/conf.d/git.fish
+++ .config/fish/conf.d/git.fish
@@ -3,7 +3,7 @@ abbr --add g git
 #abbr --add gr 'git remote -v'
 #abbr --add gpull "git pull"
 #abbr --add gpush "git push"
-abbr --add gitroot 'cd $(git rev-parse --show-toplevel)'
+abbr --add groot 'cd $(git rev-parse --show-toplevel)'
 abbr --add gsa 'git submodule add --depth=1'
 #abbr --add gsu 'git submodule update --init --depth=1'
 abbr --add gls "git pull --recurse-submodule=yes"
