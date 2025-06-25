37d8979ee9c655ad609f904ea3939771ed78a175
roc <roc@imroc.cc>
2025-06-25 14:43:17 +0800

    nvim: use leader+y to yank path

--- .config/nvim/lua/config/keymaps/file.lua
+++ .config/nvim/lua/config/keymaps/file.lua
@@ -1,11 +1,11 @@
 local clipboard = require("util.clipboard")
 
 -- copy file path
-vim.keymap.set("n", "<leader>pa", clipboard.copy_absolute_path, { desc = "[P]Copy Absolute Path" })
-vim.keymap.set("n", "<leader>pn", clipboard.copy_filename, { desc = "[P]Copy Filename" })
-vim.keymap.set("n", "<leader>pr", clipboard.copy_relative_path, { desc = "[P]Copy Relative Path" })
-vim.keymap.set("n", "<leader>pR", clipboard.copy_current_root_directory, { desc = "[P]Copy Current Root Directory" })
-vim.keymap.set("n", "<leader>pd", clipboard.copy_current_directory, { desc = "[P]Copy Current Directory" })
+vim.keymap.set("n", "<leader>yf", clipboard.copy_absolute_path, { desc = "[P]Copy Absolute Path" })
+vim.keymap.set("n", "<leader>yn", clipboard.copy_filename, { desc = "[P]Copy Filename" })
+vim.keymap.set("n", "<leader>yR", clipboard.copy_relative_path, { desc = "[P]Copy Relative Path" })
+vim.keymap.set("n", "<leader>yr", clipboard.copy_current_root_directory, { desc = "[P]Copy Current Root Directory" })
+vim.keymap.set("n", "<leader>yd", clipboard.copy_current_directory, { desc = "[P]Copy Current Directory" })
 
 -- file permission
 vim.keymap.set("n", "<leader>fx", "<cmd>!chmod +x %<cr>", { desc = "[P]Add executable permission" })
