6ce251f0ec17ea43d2107e1e992fd6fc6982896c
roc <roc@imroc.cc>
2025-06-26 11:48:28 +0800

    fish: update kubectl completions

--- .config/fish/completions/kubectl.fish
+++ .config/fish/completions/kubectl.fish
@@ -4,14 +4,14 @@ function __fish_kubectl_ns
     command kubectl get namespaces -o json | jq -r '.items[].metadata.name'
 end
 
-function __fish_kubectl_node_shell
+function __fish_kubectl_node_list_to_login
     command kubectl get node -o json | jq -r '.items[].metadata.name' | grep -v eklet-
 end
 
-function __fish_kubectl_pod_shell
+function __fish_kubectl_pod_list
     command kubectl get pod -o json | jq -r '.items[].metadata.name'
 end
 
 complete -c kubectl -f -n "__fish_seen_subcommand_from ns" -a "(__fish_kubectl_ns)" -d Namespace
-complete -c kubectl -f -n "__fish_seen_subcommand_from node-shell" -a "(__fish_kubectl_node_shell)" -d NodeShell
-complete -c kubectl -f -n "__fish_seen_subcommand_from pod-shell" -a "(__fish_kubectl_pod_shell)" -d PodShell
+complete -c kubectl -f -n "__fish_seen_subcommand_from node-shell" -a "(__fish_kubectl_node_list_to_login)" -d NodeShell
+complete -c kubectl -f -n "__fish_seen_subcommand_from pod-shell" -a "(__fish_kubectl_pod_list)" -d PodShell
