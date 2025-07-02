c3c827c60b20d8205a84acb71aa515ecfbcfad4a
roc <roc@imroc.cc>
2025-07-02 16:06:14 +0800

    nvim: update copy path shortcuts

--- .config/nvim/lua/config/keymaps/file.lua
+++ .config/nvim/lua/config/keymaps/file.lua
@@ -3,8 +3,8 @@ local clipboard = require("util.clipboard")
 -- copy file path
 vim.keymap.set("n", "<leader>yf", clipboard.copy_absolute_path, { desc = "[P]Copy Absolute Path" })
 vim.keymap.set("n", "<leader>yn", clipboard.copy_filename, { desc = "[P]Copy Filename" })
-vim.keymap.set("n", "<leader>yR", clipboard.copy_relative_path, { desc = "[P]Copy Relative Path" })
-vim.keymap.set("n", "<leader>yr", clipboard.copy_current_root_directory, { desc = "[P]Copy Current Root Directory" })
+vim.keymap.set("n", "<leader>yr", clipboard.copy_relative_path, { desc = "[P]Copy Relative Path" })
+vim.keymap.set("n", "<leader>yR", clipboard.copy_current_root_directory, { desc = "[P]Copy Current Root Directory" })
 vim.keymap.set("n", "<leader>yd", clipboard.copy_current_directory, { desc = "[P]Copy Current Directory" })
 
 -- file permission
