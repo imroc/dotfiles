95d0ccf72f6110ae41ffa2f1031078d78f6c12a6
roc <roc@imroc.cc>
2025-07-08 14:10:32 +0800

    nvim: refactor markdown

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,144 +1,24 @@
 vim.opt_local.wrap = true
 
--- Function to fold all headings of a specific level
-local function fold_headings_of_level(level)
-  -- Move to the top of the file
-  vim.cmd("normal! gg")
-  -- Get the total number of lines
-  local total_lines = vim.fn.line("$")
-  for line = 1, total_lines do
-    -- Get the content of the current line
-    local line_content = vim.fn.getline(line)
-    -- "^" -> Ensures the match is at the start of the line
-    -- string.rep("#", level) -> Creates a string with 'level' number of "#" characters
-    -- "%s" -> Matches any whitespace character after the "#" characters
-    -- So this will match `## `, `### `, `#### ` for example, which are markdown headings
-    if line_content:match("^" .. string.rep("#", level) .. "%s") then
-      -- Move the cursor to the current line
-      vim.fn.cursor(line, 1)
-      -- Fold the heading if it matches the level
-      if vim.fn.foldclosed(line) == -1 then
-        vim.cmd("normal! za")
-      end
-    end
-  end
-end
+local map = vim.keymap.set
+local md = require("util.markdown")
 
-local function fold_markdown_headings(levels)
-  -- I save the view to know where to jump back after folding
-  local saved_view = vim.fn.winsaveview()
-  for _, level in ipairs(levels) do
-    fold_headings_of_level(level)
-  end
-  vim.cmd("nohlsearch")
-  -- Restore the view to jump to where I was
-  vim.fn.winrestview(saved_view)
-end
+map({ "n", "v" }, "gk", md.goto_previous_header, { buffer = 0, desc = "[P]Go to previous markdown header" })
 
--- Use <CR> to fold when in normal mode
--- To see help about folds use `:help fold`
--- vim.keymap.set("n", "<CR>", function()
---   -- Get the current line number
---   local line = vim.fn.line(".")
---   -- Get the fold level of the current line
---   local foldlevel = vim.fn.foldlevel(line)
---   if foldlevel == 0 then
---     vim.notify("No fold found", vim.log.levels.INFO)
---   else
---     vim.cmd("normal! za")
---   end
--- end, { desc = "[P]Toggle fold" })
+map({ "n", "v" }, "gj", md.goto_next_header, { buffer = 0, desc = "[P]Go to next markdown header" })
 
--- Search UP for a markdown header
--- Make sure to follow proper markdown convention, and you have a single H1
--- heading at the very top of the file
--- This will only search for H2 headings and above
-vim.keymap.set({ "n", "v" }, "gk", function()
-  -- `?` - Start a search backwards from the current cursor position.
-  -- `^` - Match the beginning of a line.
-  -- `##` - Match 2 ## symbols
-  -- `\\+` - Match one or more occurrences of prev element (#)
-  -- `\\s` - Match exactly one whitespace character following the hashes
-  -- `.*` - Match any characters (except newline) following the space
-  -- `$` - Match extends to end of line
-  vim.cmd("silent! ?^##\\+\\s.*$")
-  -- Clear the search highlight
-  vim.cmd("nohlsearch")
-end, { buffer = 0, desc = "[P]Go to previous markdown header" })
+map("n", "zu", md.unfold_level_2, { buffer = 0, desc = "[P]Unfold all headings level 2 or above" })
 
--- Search DOWN for a markdown header
--- Make sure to follow proper markdown convention, and you have a single H1
--- heading at the very top of the file
--- This will only search for H2 headings and above
-vim.keymap.set({ "n", "v" }, "gj", function()
-  -- `/` - Start a search forwards from the current cursor position.
-  -- `^` - Match the beginning of a line.
-  -- `##` - Match 2 ## symbols
-  -- `\\+` - Match one or more occurrences of prev element (#)
-  -- `\\s` - Match exactly one whitespace character following the hashes
-  -- `.*` - Match any characters (except newline) following the space
-  -- `$` - Match extends to end of line
-  vim.cmd("silent! /^##\\+\\s.*$")
-  -- Clear the search highlight
-  vim.cmd("nohlsearch")
-end, { buffer = 0, desc = "[P]Go to next markdown header" })
+map("n", "zi", md.fold_current, { buffer = 0, desc = "[P]Fold the heading cursor currently on" })
 
--- Keymap for unfolding markdown headings of level 2 or above
--- Changed all the markdown folding and unfolding keymaps from <leader>mfj to
--- zj, zk, zl, z; and zu respectively lamw25wmal
-vim.keymap.set("n", "zu", function()
-  -- vim.keymap.set("n", "<leader>mfu", function()
-  -- Reloads the file to reflect the changes
-  vim.cmd("edit!")
-  vim.cmd("normal! zR") -- Unfold all headings
-end, { buffer = 0, desc = "[P]Unfold all headings level 2 or above" })
+map("n", "zj", md.fold_level_1, { buffer = 0, desc = "[P]Fold all headings level 1 or above" })
 
--- gk jummps to the markdown heading above and then folds it
--- zi by default toggles folding, but I don't need it lamw25wmal
-vim.keymap.set("n", "zi", function()
-  -- Difference between normal and normal!
-  -- - `normal` executes the command and respects any mappings that might be defined.
-  -- - `normal!` executes the command in a "raw" mode, ignoring any mappings.
-  vim.cmd("normal gk")
-  -- This is to fold the line under the cursor
-  vim.cmd("normal! za")
-end, { buffer = 0, desc = "[P]Fold the heading cursor currently on" })
+map("n", "zk", md.fold_level_2, { buffer = 0, desc = "[P]Fold all headings level 2 or above" })
 
--- Keymap for folding markdown headings of level 1 or above
-vim.keymap.set("n", "zj", function()
-  -- vim.keymap.set("n", "<leader>mfj", function()
-  -- Reloads the file to refresh folds, otherwise you have to re-open neovim
-  vim.cmd("edit!")
-  -- Unfold everything first or I had issues
-  vim.cmd("normal! zR")
-  fold_markdown_headings({ 6, 5, 4, 3, 2, 1 })
-end, { buffer = 0, desc = "[P]Fold all headings level 1 or above" })
+map("n", "zl", md.fold_level_3, { buffer = 0, desc = "[P]Fold all headings level 3 or above" })
 
--- Keymap for folding markdown headings of level 2 or above
--- I know, it reads like "madafaka" but "k" for me means "2"
-vim.keymap.set("n", "zk", function()
-  -- vim.keymap.set("n", "<leader>mfk", function()
-  -- Reloads the file to refresh folds, otherwise you have to re-open neovim
-  vim.cmd("edit!")
-  -- Unfold everything first or I had issues
-  vim.cmd("normal! zR")
-  fold_markdown_headings({ 6, 5, 4, 3, 2 })
-end, { buffer = 0, desc = "[P]Fold all headings level 2 or above" })
-
--- Keymap for folding markdown headings of level 3 or above
-vim.keymap.set("n", "zl", function()
-  -- vim.keymap.set("n", "<leader>mfl", function()
-  -- Reloads the file to refresh folds, otherwise you have to re-open neovim
-  vim.cmd("edit!")
-  -- Unfold everything first or I had issues
-  vim.cmd("normal! zR")
-  fold_markdown_headings({ 6, 5, 4, 3 })
-end, { buffer = 0, desc = "[P]Fold all headings level 3 or above" })
+map("n", "<localleader>w", "", { buffer = 0, desc = "+[P]iwiki" })
 
 local iwiki = require("util.iwiki")
-vim.keymap.set("n", "<localleader>w", function()
-  iwiki.save_iwiki()
-end, { buffer = 0, desc = "[P]Sync to iwiki" })
-vim.keymap.set("n", "<localleader>W", function()
-  iwiki.open_iwiki()
-end, { buffer = 0, desc = "[P]Open iwiki in browser" })
+map("n", "<localleader>ws", iwiki.save_iwiki, { buffer = 0, desc = "[P]Save" })
+map("n", "<localleader>wo", iwiki.open_iwiki, { buffer = 0, desc = "[P]Open in browser" })
---
+++ .config/nvim/lua/util/markdown.lua
@@ -0,0 +1,137 @@
+---@diagnostic disable: missing-fields
+
+-- Function to fold all headings of a specific level
+local function fold_headings_of_level(level)
+  -- Move to the top of the file
+  vim.cmd("normal! gg")
+  -- Get the total number of lines
+  local total_lines = vim.fn.line("$")
+  for line = 1, total_lines do
+    -- Get the content of the current line
+    local line_content = vim.fn.getline(line)
+    -- "^" -> Ensures the match is at the start of the line
+    -- string.rep("#", level) -> Creates a string with 'level' number of "#" characters
+    -- "%s" -> Matches any whitespace character after the "#" characters
+    -- So this will match `## `, `### `, `#### ` for example, which are markdown headings
+    if line_content:match("^" .. string.rep("#", level) .. "%s") then
+      -- Move the cursor to the current line
+      vim.fn.cursor(line, 1)
+      -- Fold the heading if it matches the level
+      if vim.fn.foldclosed(line) == -1 then
+        vim.cmd("normal! za")
+      end
+    end
+  end
+end
+
+local function fold_markdown_headings(levels)
+  -- I save the view to know where to jump back after folding
+  local saved_view = vim.fn.winsaveview()
+  for _, level in ipairs(levels) do
+    fold_headings_of_level(level)
+  end
+  vim.cmd("nohlsearch")
+  -- Restore the view to jump to where I was
+  vim.fn.winrestview(saved_view)
+end
+
+-- Use <CR> to fold when in normal mode
+-- To see help about folds use `:help fold`
+-- vim.keymap.set("n", "<CR>", function()
+--   -- Get the current line number
+--   local line = vim.fn.line(".")
+--   -- Get the fold level of the current line
+--   local foldlevel = vim.fn.foldlevel(line)
+--   if foldlevel == 0 then
+--     vim.notify("No fold found", vim.log.levels.INFO)
+--   else
+--     vim.cmd("normal! za")
+--   end
+-- end, { desc = "[P]Toggle fold" })
+
+local M = {}
+
+-- Search UP for a markdown header
+-- Make sure to follow proper markdown convention, and you have a single H1
+-- heading at the very top of the file
+-- This will only search for H2 headings and above
+function M.goto_previous_header()
+  -- `?` - Start a search backwards from the current cursor position.
+  -- `^` - Match the beginning of a line.
+  -- `##` - Match 2 ## symbols
+  -- `\\+` - Match one or more occurrences of prev element (#)
+  -- `\\s` - Match exactly one whitespace character following the hashes
+  -- `.*` - Match any characters (except newline) following the space
+  -- `$` - Match extends to end of line
+  vim.cmd("silent! ?^##\\+\\s.*$")
+  -- Clear the search highlight
+  vim.cmd("nohlsearch")
+end
+
+-- Search DOWN for a markdown header
+-- Make sure to follow proper markdown convention, and you have a single H1
+-- heading at the very top of the file
+-- This will only search for H2 headings and above
+function M.goto_next_header()
+  -- `/` - Start a search forwards from the current cursor position.
+  -- `^` - Match the beginning of a line.
+  -- `##` - Match 2 ## symbols
+  -- `\\+` - Match one or more occurrences of prev element (#)
+  -- `\\s` - Match exactly one whitespace character following the hashes
+  -- `.*` - Match any characters (except newline) following the space
+  -- `$` - Match extends to end of line
+  vim.cmd("silent! /^##\\+\\s.*$")
+  -- Clear the search highlight
+  vim.cmd("nohlsearch")
+end
+
+-- Unfold markdown headings of level 2 or above
+-- Changed all the markdown folding and unfolding keymaps from <leader>mfj to
+-- zj, zk, zl, z; and zu respectively lamw25wmal
+function M.unfold_level_2()
+  -- vim.keymap.set("n", "<leader>mfu", function()
+  -- Reloads the file to reflect the changes
+  vim.cmd("edit!")
+  vim.cmd("normal! zR") -- Unfold all headings
+end
+
+-- gk jummps to the markdown heading above and then folds it
+-- zi by default toggles folding, but I don't need it lamw25wmal
+function M.fold_current()
+  M.goto_previous_header()
+  -- This is to fold the line under the cursor
+  vim.cmd("normal! za")
+end
+
+-- fold markdown headings of level 1 or above
+function M.fold_level_1()
+  -- vim.keymap.set("n", "<leader>mfj", function()
+  -- Reloads the file to refresh folds, otherwise you have to re-open neovim
+  vim.cmd("edit!")
+  -- Unfold everything first or I had issues
+  vim.cmd("normal! zR")
+  fold_markdown_headings({ 6, 5, 4, 3, 2, 1 })
+end
+
+-- fold markdown headings of level 2 or above
+-- I know, it reads like "madafaka" but "k" for me means "2"
+function M.fold_level_2()
+  -- vim.keymap.set("n", "<leader>mfk", function()
+  -- Reloads the file to refresh folds, otherwise you have to re-open neovim
+  vim.cmd("edit!")
+  -- Unfold everything first or I had issues
+  vim.cmd("normal! zR")
+  fold_markdown_headings({ 6, 5, 4, 3, 2 })
+end
+
+-- fold markdown headings of level 3 or above
+function M.fold_level_3()
+  -- vim.keymap.set("n", "<leader>mfl", function()
+  -- Reloads the file to refresh folds, otherwise you have to re-open neovim
+  vim.cmd("edit!")
+  -- Unfold everything first or I had issues
+  vim.cmd("normal! zR")
+  fold_markdown_headings({ 6, 5, 4, 3 })
+end
+
+return M
