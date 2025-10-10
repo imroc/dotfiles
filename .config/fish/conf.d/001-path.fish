c02bc305dacbd5f6f63958ad033296039979f39c
roc <roc@imroc.cc>
2025-10-10 16:55:45 +0800

    fish: add llvm path

--- .config/fish/conf.d/001-path.fish
+++ .config/fish/conf.d/001-path.fish
@@ -12,6 +12,7 @@ fish_add_path -m \
     /home/linuxbrew/.linuxbrew/bin \
     /home/linuxbrew/.linuxbrew/sbin \
     /opt/homebrew/opt/make/libexec/gnubin \
+    /opt/homebrew/opt/llvm/bin \
     /opt/maven/bin \
     /nix/var/nix/profiles/default/bin \
     $HOME/.nix-profile/bin
