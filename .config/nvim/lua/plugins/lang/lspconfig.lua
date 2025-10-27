bf0f5a0c0aceb5c5adb0317413e82ef102f56e0d
roc <roc@imroc.cc>
2025-10-27 09:46:49 +0800

    nvim: remove lspconfig

--- .config/nvim/lua/plugins/lang/lspconfig.lua
+++ .config/nvim/lua/plugins/lang/lspconfig.lua
@@ -1,9 +1,4 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
-    -- diable <M-n> and <M-p> (avoid conflict with next/prev change)
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "<a-n>", false }
-    keys[#keys + 1] = { "<a-p>", false }
-  end,
+  opts = {},
 }
