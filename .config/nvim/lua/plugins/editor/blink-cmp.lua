2752f8f6f3cd995eb9aa50ae29d7a82d008ccf83
roc <roc@imroc.cc>
2025-11-24 12:12:30 +0800

    nvim: do not use enter to accept

--- .config/nvim/lua/plugins/editor/blink-cmp.lua
+++ .config/nvim/lua/plugins/editor/blink-cmp.lua
@@ -2,6 +2,7 @@ return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
+      preset = "default",
       ["<M-space>"] = { "show", "show_documentation", "hide_documentation" },
       ["<C-k>"] = { "select_prev" },
       ["<C-j>"] = { "select_next" },
