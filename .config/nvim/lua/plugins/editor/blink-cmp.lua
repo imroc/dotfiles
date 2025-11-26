1098e735193ef39acc6ea01767dacab359e94b8e
roc <roc@imroc.cc>
2025-11-26 21:23:16 +0800

    nvim: remove blink default preset

--- .config/nvim/lua/plugins/editor/blink-cmp.lua
+++ .config/nvim/lua/plugins/editor/blink-cmp.lua
@@ -2,7 +2,6 @@ return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
-      preset = "default",
       ["<M-space>"] = { "show", "show_documentation", "hide_documentation" },
       ["<C-k>"] = { "select_prev" },
       ["<C-j>"] = { "select_next" },
