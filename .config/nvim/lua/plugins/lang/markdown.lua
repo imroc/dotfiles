0e57f555e8b7e17b6d1862f5a0b6cc77548c60eb
roc <roc@imroc.cc>
2025-11-27 12:50:52 +0800

    nvim: update markdown render

--- .config/nvim/lua/plugins/lang/markdown.lua
+++ .config/nvim/lua/plugins/lang/markdown.lua
@@ -69,6 +69,13 @@ return {
         desc = "[P]Update Numbering For List Item",
       },
       {
+        "<localleader>d",
+        mode = { "n", "v" },
+        "<cmd>MkdnToggleToDo<cr>",
+        ft = "markdown",
+        desc = "[P]Toggle ToDo",
+      },
+      {
         "<localleader>t",
         function()
           vim.ui.input({ prompt = "Create Table: nrow ncol" }, function(msg)
@@ -89,6 +96,11 @@ return {
         foldtext = false,
         bib = false,
         conceal = false,
+      },
+      to_do = {
+        symbols = { " ", ">", "x" },
+        in_progress = ">",
+        complete = "x",
       },
     },
   },
@@ -208,22 +220,44 @@ return {
         highlight_inline = "RenderMarkdownCodeInfo",
       },
       heading = {
-        icons = { " 󰼏 ", " 󰎨 ", " 󰼑 ", " 󰎲 ", " 󰼓 ", " 󰎴 " },
-        border = true,
+        icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
         render_modes = true, -- keep rendering while inserting
       },
       checkbox = {
         -- Turn on / off checkbox state rendering
         enabled = true,
-        unchecked = {
-          icon = "󰄱",
-          highlight = "RenderMarkdownCodeFallback",
-          scope_highlight = "RenderMarkdownCodeFallback",
+        checkbox = {
+          -- Turn on / off checkbox state rendering
+          enabled = true,
+          -- Determines how icons fill the available space:
+          --  inline:  underlying text is concealed resulting in a left aligned icon
+          --  overlay: result is left padded with spaces to hide any additional text
+          position = "inline",
+          bullet = false,
+          unchecked = {
+            -- Replaces '[ ]' of 'task_list_marker_unchecked'
+            icon = "   󰄱 ",
+            -- Highlight for the unchecked icon
+            highlight = "RenderMarkdownUnchecked",
+            -- Highlight for item associated with unchecked checkbox
+            scope_highlight = nil,
+          },
+          checked = {
+            -- Replaces '[x]' of 'task_list_marker_checked'
+            icon = "   󰱒 ",
+            -- Highlight for the checked icon
+            highlight = "RenderMarkdownChecked",
+            -- Highlight for item associated with checked checkbox
+            scope_highlight = nil,
+          },
         },
-        checked = {
-          icon = "󰄵",
-          highlight = "RenderMarkdownUnchecked",
-          scope_highlight = "RenderMarkdownUnchecked",
+        html = {
+          -- Turn on / off all HTML rendering
+          enabled = true,
+          comment = {
+            -- Turn on / off HTML comment concealing
+            conceal = false,
+          },
         },
         custom = {
           question = {
