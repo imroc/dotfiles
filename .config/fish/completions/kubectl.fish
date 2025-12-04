7956a468a73858c3eea246d6d116f51690fecb1a
roc <roc@imroc.cc>
2025-12-04 11:38:23 +0800

    update config

--- .config/fish/completions/kubectl.fish
+++ .config/fish/completions/kubectl.fish
@@ -1,15 +1,15 @@
 kubectl completion fish 2>/dev/null | source
 
 function __fish_kubectl_ns
-    command kubectl get namespaces -o json | jq -r '.items[].metadata.name'
+    kubectl get namespaces -o json | jq -r '.items[].metadata.name'
 end
 
 function __fish_kubectl_node_list_to_login
-    command kubectl get node -o json | jq -r '.items[].metadata.name' | grep -v eklet-
+    kubectl get node -o json | jq -r '.items[].metadata.name' | grep -v eklet-
 end
 
 function __fish_kubectl_pod_list
-    command kubectl get pod -o json | jq -r '.items[].metadata.name'
+    kubectl get pod -o json | jq -r '.items[].metadata.name'
 end
 
 complete -c kubectl -f -n "__fish_seen_subcommand_from ns" -a "(__fish_kubectl_ns)" -d Namespace
