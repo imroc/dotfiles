20a690863cb21f441fa68a22a06aab05c5148f93
roc <roc@imroc.cc>
2025-12-09 14:22:27 +0800

    update nvim keys

--- .config/nvim/lua/plugins/editor/floaterm.lua
+++ .config/nvim/lua/plugins/editor/floaterm.lua
@@ -10,5 +10,11 @@ return {
       mode = { "n", "v", "i", "t" },
       desc = "[P] Toggle Floaterm",
     },
+    {
+      "<C-t>",
+      "<cmd>FloatermToggle<cr>",
+      mode = { "n", "v", "t" },
+      desc = "[P] Toggle Floaterm",
+    },
   },
 }
