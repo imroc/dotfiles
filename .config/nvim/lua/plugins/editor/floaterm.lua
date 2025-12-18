575ae06de3edbd4d419f096ee80bebb979184bca
roc <roc@imroc.cc>
2025-12-18 09:52:02 +0800

    update config
    Signed-off-by: roc <roc@imroc.cc>

--- .config/nvim/lua/plugins/editor/floaterm.lua
+++ .config/nvim/lua/plugins/editor/floaterm.lua
@@ -5,12 +5,6 @@ return {
   cmd = "FloatermToggle",
   keys = {
     {
-      "<C-;>",
-      "<cmd>FloatermToggle<cr>",
-      mode = { "n", "v", "i", "t" },
-      desc = "[P] Toggle Floaterm",
-    },
-    {
       "<C-t>",
       "<cmd>FloatermToggle<cr>",
       mode = { "n", "v", "t" },
