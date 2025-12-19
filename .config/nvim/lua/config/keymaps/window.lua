d61869f0e74baa0b60b9bc0ba8568930fefecff0
roc <roc@imroc.cc>
2025-12-19 12:20:17 +0800

    update nvim config

--- .config/nvim/lua/config/keymaps/window.lua
+++ .config/nvim/lua/config/keymaps/window.lua
@@ -1,7 +1,4 @@
 local window = require("util.window")
 
 vim.keymap.set({ "n", "t" }, "<C-'>", "<cmd>close<cr>", { desc = "[P]Close Window" })
-vim.keymap.set({ "n", "t" }, "<C-x>", "<cmd>close<cr>", { desc = "[P]Close Window" })
-vim.keymap.set({ "n" }, "<C-_>", "<C-W>s", { desc = "[P]Split Window Below", remap = true })
-vim.keymap.set({ "n" }, "<C-\\>", "<C-W>v", { desc = "[P]Split Window Right", remap = true })
 vim.keymap.set("n", "<leader>j", window.clear, { desc = "[P]Clear all other windows" })
