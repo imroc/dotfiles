80176776ee15cb07f0e05fbfd89e109151a7c44e
roc <roc@imroc.cc>
2025-12-22 23:14:30 +0800

    update nvim config

--- .config/nvim/lua/plugins/editor/im-select.lua
+++ .config/nvim/lua/plugins/editor/im-select.lua
@@ -11,5 +11,6 @@ return {
   opts = {
     default_im_select = "com.apple.keylayout.ABC",
     default_command = "macism",
+    set_default_events = { "InsertLeave", "CmdlineLeave", "TermLeave" },
   },
 }
