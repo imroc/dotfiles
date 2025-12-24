5fc5674d4c072c1b29846b5e72699dd44394570d
roc <roc@imroc.cc>
2025-12-24 10:17:28 +0800

    update nvim config

--- .config/nvim/lua/plugins/editor/toggleterm.lua
+++ .config/nvim/lua/plugins/editor/toggleterm.lua
@@ -1,6 +1,6 @@
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
-  vim.keymap.set("t", "<C-.>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-;>", [[<C-\><C-n>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
