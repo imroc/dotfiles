82224ca44ebb776cd20e925d61a8fee1ff0dc98e
roc <roc@imroc.cc>
2026-01-21 17:43:09 +0800

    update

--- .config/fish/conf.d/editor.fish
+++ .config/fish/conf.d/editor.fish
@@ -3,4 +3,4 @@ abbr --add yy "cat > $HOME/tmp/kube.yaml; and nvim $HOME/tmp/kube.yaml"
 abbr --add y 'nvim -c "set filetype=yaml"'
 abbr --add c "code -r"
 abbr --add v nvim
-abbr --add rns "rm $HOME/.local/state/nvim/swap/*"
+abbr --add cnc "rm $HOME/.local/state/nvim/swap/* && rm $HOME/.local/state/nvim/shada/*"
