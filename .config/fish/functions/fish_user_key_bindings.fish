303da7faeec41a87f17f49943662a5e26bf15850
roc <roc@imroc.cc>
2026-01-28 17:24:31 +0800

    fish: remove fish_user_key_bindings
    Signed-off-by: roc <roc@imroc.cc>

--- .config/fish/functions/fish_user_key_bindings.fish
+++
@@ -1,7 +0,0 @@
-function fish_user_key_bindings
-    # Alt+I/O 后退/前进一个 bigword
-    bind $argv alt-i backward-bigword
-    bind $argv alt-o forward-bigword
-    # 绑定 tab 为自动补全
-    bind tab complete-and-search
-end
