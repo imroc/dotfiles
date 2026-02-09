de68c2914dc36d70b4d511a6470964880f4f8307
roc <roc@imroc.cc>
2026-02-09 16:47:18 +0800

    fish: update alias

--- .config/fish/conf.d/ai.fish
+++ .config/fish/conf.d/ai.fish
@@ -6,5 +6,6 @@ abbr --add cr ccr code
 abbr --add oc opencode
 abbr --add ow openclaw
 abbr --add ot openclaw tui
+abbr --add ol openclaw logs --follow
 abbr --add og openclaw gateway
 abbr --add ogr openclaw gateway restart
