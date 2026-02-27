c188a72e4af99a64155a08a92cc15cb2d2b1de7c
roc <roc@imroc.cc>
2026-02-27 10:43:24 +0800

    nvim: update yadm

--- .config/nvim/lua/util/yadm.lua
+++ .config/nvim/lua/util/yadm.lua
@@ -1,13 +1,22 @@
 local M = {}
 
--- use lazygit to open dotfiles which managed by yadm
+local function git_dir(type)
+  return vim.fn.expand("$HOME/.local/share/yadm-" .. type .. "/repo.git")
+end
+
+local function yadm_dir(type)
+  return vim.fn.expand("$HOME/.config/yadm-" .. type)
+end
+
+local function work_tree()
+  return vim.fn.expand("$HOME")
+end
+
 function M.open_lazygit(type)
   return function()
     local lazygit_config_file = vim.fn.expand("$HOME/.config/yadm/lazygit.yml")
-    local work_tree = vim.fn.expand("$HOME")
-    local git_dir = vim.fn.expand("$HOME/.local/share/yadm-" .. type .. "/repo.git")
     Snacks.terminal(
-      { "lazygit", "--use-config-file", lazygit_config_file, "--work-tree", work_tree, "--git-dir", git_dir },
+      { "lazygit", "--use-config-file", lazygit_config_file, "--work-tree", work_tree(), "--git-dir", git_dir(type) },
       { win = { width = 0, height = 0 } }
     )
   end
@@ -19,8 +28,8 @@ local current_git_dir = nil
 function M.set_git_env(type)
   current_git_work_tree = vim.env.GIT_WORK_TREE
   current_git_dir = vim.env.GIT_DIR
-  vim.env.GIT_WORK_TREE = vim.fn.expand("$HOME")
-  vim.env.GIT_DIR = vim.fn.expand("$HOME/.local/share/yadm-" .. type .. "/repo.git")
+  vim.env.GIT_WORK_TREE = work_tree()
+  vim.env.GIT_DIR = git_dir(type)
 end
 
 function M.remove_git_env()
@@ -28,13 +37,19 @@ function M.remove_git_env()
   vim.env.GIT_DIR = current_git_dir
 end
 
--- add file to yadm
-function M.git_add(filepath)
-  vim.fn.system({ "yadm", "add", filepath })
-  if vim.v.shell_error == 0 then
-    vim.notify(string.format("Added to yadm: %s", filepath), vim.log.levels.INFO)
-  else
-    vim.notify(string.format("Failed to add to yadm: %s", filepath), vim.log.levels.ERROR)
+function M.add_current_file(type)
+  return function()
+    local filepath = vim.fn.expand("%:p")
+    if filepath == "" then
+      vim.notify("No file in current buffer", vim.log.levels.WARN)
+      return
+    end
+    vim.fn.system({ "yadm", "--yadm-dir", yadm_dir(type), "--yadm-data", git_dir(type), "add", filepath })
+    if vim.v.shell_error == 0 then
+      vim.notify(string.format("Yadm added (%s): %s", type, filepath), vim.log.levels.INFO)
+    else
+      vim.notify(string.format("Yadm add failed (%s): %s", type, filepath), vim.log.levels.ERROR)
+    end
   end
 end
 
