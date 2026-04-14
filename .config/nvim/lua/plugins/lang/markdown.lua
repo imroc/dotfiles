4996ba3848e9381ed45ec1ee3583772c11db1a9a
roc <roc@imroc.cc>
2026-04-14 20:26:34 +0800

    update nvim config

--- .config/nvim/lua/plugins/lang/markdown.lua
+++ .config/nvim/lua/plugins/lang/markdown.lua
@@ -35,6 +35,10 @@ return {
     keys = {
       { "<localleader>o", "<cmd>MarkdownPreviewToggle<cr>", ft = "markdown", desc = "[P]Toggle Preview" },
     },
+    init = function()
+      vim.g.mkdp_markdown_css = vim.fn.expand("~/.config/nvim/resources/markdown-preview/github-markdown-light.css")
+      vim.g.mkdp_highlight_css = vim.fn.expand("~/.config/nvim/resources/markdown-preview/github-dark.css")
+    end,
   },
   {
     "Kicamon/markdown-table-mode.nvim",
