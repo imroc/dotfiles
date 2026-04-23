03204bdf2541b536ce2b23ecd091cb5aef309150
roc <roc@imroc.cc>
2026-04-23 11:36:30 +0800

    nvim: update navbuddy

--- .config/nvim/lua/plugins/lang/lspconfig.lua
+++ .config/nvim/lua/plugins/lang/lspconfig.lua
@@ -1,5 +1,24 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    {
+      "SmiteshP/nvim-navbuddy",
+      keys = {
+        {
+          "<leader>cs",
+          function()
+            require("nvim-navbuddy").open()
+          end,
+          desc = "[P]Symbols Outline (Float)",
+        },
+      },
+      dependencies = {
+        "SmiteshP/nvim-navic",
+        "MunifTanjim/nui.nvim",
+      },
+      opts = { lsp = { auto_attach = true } },
+    },
+  },
   opts = {
     servers = {
       ["*"] = {
