cb88a6b766f62614e576258713e9566a67378bf3
roc <roc@imroc.cc>
2026-04-28 18:51:50 +0800

    nvim: remove obsidian.nvim

--- .config/nvim/lua/plugins/lang/markdown.lua
+++ .config/nvim/lua/plugins/lang/markdown.lua
@@ -412,23 +412,23 @@ return {
       },
     },
   },
-  {
-    "obsidian-nvim/obsidian.nvim",
-    version = "*", -- use latest release, remove to use latest commit
-    ft = "markdown",
-    ---@module 'obsidian'
-    ---@type obsidian.config
-    opts = {
-      legacy_commands = false, -- this will be removed in the next major release
-      workspaces = {
-        {
-          name = "work",
-          path = "~/work",
-        },
-      },
-      frontmatter = {
-        enabled = false,
-      },
-    },
-  },
+  -- {
+  --   "obsidian-nvim/obsidian.nvim",
+  --   version = "*", -- use latest release, remove to use latest commit
+  --   ft = "markdown",
+  --   ---@module 'obsidian'
+  --   ---@type obsidian.config
+  --   opts = {
+  --     legacy_commands = false, -- this will be removed in the next major release
+  --     workspaces = {
+  --       {
+  --         name = "work",
+  --         path = "~/work",
+  --       },
+  --     },
+  --     frontmatter = {
+  --       enabled = false,
+  --     },
+  --   },
+  -- },
 }
