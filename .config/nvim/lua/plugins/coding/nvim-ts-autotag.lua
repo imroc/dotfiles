47884e451c3dec37f30565514889ceafc05876df
roc <roc@imroc.cc>
2026-04-30 17:29:31 +0800

    nvim-ts-autotag: 禁用 markdown 文件中的自动标签闭合
    在 md 文件中输入 <xx> 后不再自动补全 </xx>，避免干扰 markdown 中的非 HTML 标签内容编写。
    通过 per_filetype 配置仅对 markdown 禁用 enable_close，其他文件类型不受影响。

--- .config/nvim/lua/plugins/coding/nvim-ts-autotag.lua
+++ .config/nvim/lua/plugins/coding/nvim-ts-autotag.lua
@@ -2,7 +2,13 @@ return {
   {
     "windwp/nvim-ts-autotag",
     config = function()
-      require("nvim-ts-autotag").setup()
+      require("nvim-ts-autotag").setup({
+        per_filetype = {
+          ["markdown"] = {
+            enable_close = false,
+          },
+        },
+      })
     end,
     lazy = true,
     event = "InsertEnter",
